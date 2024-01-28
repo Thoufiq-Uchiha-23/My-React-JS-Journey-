a14c18df8a275a6a94a8b0ffcf7f1805b37ca42a
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-01-28 20:56:10 +0530

    added comments

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -4,23 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // setting the states for the objects
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
 
+  // passing ref in default as null
   const passwordRef = useRef(null)
 
+  // to store some cache and change acc to loading 
   const generatePassword = useCallback(()=>{
     let pass = ""
     let str = 
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+    // if true add nos to str
     if(numberAllowed) str += "0123456789"
+    // if true add char to string
     if(charAllowed) str += "!@#$%^&*()_+"
 
     for (let i = 1; i < length; i++) {
+      // generating random with str
       const char = Math.floor(Math.random() * str.length + 1)
+      // adding the password of str by incrementing
       pass +=  str.charAt(char)
     }
 
@@ -28,10 +35,13 @@ function App() {
   }, [length, numberAllowed, charAllowed])
 
   const copPasswordToClipboard = ()=>{
+    // to copy to clipboard
     window.navigator.clipboard.writeText(password)
+    //to select the text with blue feedback
     passwordRef.current?.select()
   }
 
+  // changes screen everytime it loads
   useEffect(()=>{
     generatePassword()
   },[length, numberAllowed, charAllowed])
