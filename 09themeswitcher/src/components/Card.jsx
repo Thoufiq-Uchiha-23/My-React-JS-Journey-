15fdef7965e89e020669c8828dc631cb4f8c2a03
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-02-03 15:45:34 +0530

    Made Cuisino dishes web page

--- 09themeswitcher/src/components/Card.jsx
+++ 09themeswitcher/src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Card() {
+function Card({image, title,price}) {
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+        <div className="w-full col-4 relative bg-red-500 border-none shadow-yellow-200 rounded-2xl dark:bg-gray-800">
+            <a href="/" className='rounded-lg'>
+                <img className="md:p-7 p-4 h-[150px] object-cover md:h-[200px] w-full rounded-2xl" src={image} alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                    <h5 className="text-md md:text-2xl font-semibold md:font-bold h-[70px] md:h-[100px] tracking-tight text-gray-900 dark:text-white">
+                        {title}
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -62,11 +62,11 @@ function Card() {
                         4.0
                     </span>
                 </div>
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                <div className="flex items-center justify-center md:justify-between flex-wrap flex-col md:flex-row gap-4 ">
+                    <span className="text-2xl text-center md:text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
                     <a
                         href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white  w-full font-bold bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 rounded-xl text-xs md:text-sm px-2 py-2 md:px-5 md:py-2.5 text-center dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-700"
                     >
                         Add to cart
                     </a>
---
+++ 09themeswitcher/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='tracking-wider font-extrabold h-[70px] w-full flex items-center justify-center md:text-4xl text-3xl bg-gray-700 text-yellow-500 absolute top-0 z-40'>
+      Cuisino
+    </div>
+  )
+}
+
+export default Header
