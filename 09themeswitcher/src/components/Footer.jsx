5a8ff929c5b82d4f8829b05e1199e7f6a8606c53
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-02-03 15:47:01 +0530

    just edited footer

--- 09themeswitcher/src/components/Footer.jsx
+++ 09themeswitcher/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex justify-evenly  flex-wrap items-center font-extrabold p-7 h-100 w-full mt-4 flex items-center justify-center md:text-4xl text-2xl bg-gray-700 text-yellow-500 relative bottom-0'>
+    <div className='flex justify-evenly  flex-wrap items-center font-extrabold p-7 h-100 w-full mt-4 md:text-4xl text-2xl bg-gray-700 text-yellow-500 relative bottom-0'>
         <img src="https://avatars.githubusercontent.com/u/143873191?v=4" className='w-[100px] rounded-full' alt="" />
         <h2>Made By <span className='hover:text-emerald-500'>Thoufiq</span></h2>
     </div>
